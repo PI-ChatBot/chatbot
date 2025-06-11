115d6f16064a5b27fe680f5f3ef93dae76d5c04c
Kilio1 <hyuri.gc@gmail.com>
2025-06-11 00:17:34 -0300

    editor de cardápio

--- app/cardapio.tsx
+++ app/cardapio.tsx
@@ -1,67 +1,102 @@
 import React, { useEffect, useState } from 'react';
-import { Animated, Image, FlatList, useWindowDimensions, StyleSheet, Dimensions, SafeAreaView, TouchableOpacity, View, Text, ScrollView } from 'react-native';
+import { Animated, Image, Switch, Modal, TextInput, FlatList, useWindowDimensions, StyleSheet, Dimensions, SafeAreaView, TouchableOpacity, View, Text, ScrollView } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
 import { formatDateTime } from './utils/formatDateTime';
 import { storage } from './utils/storage';
+import ConfirmModal from './utils/ConfirmModal';
+import * as ImagePicker from 'expo-image-picker';
 
-const pratos1 = [
+const cardapio1 = [
     { id: '1', 
       nome: 'Feijoada',
-      preco: 'R$ 35,00',
-      imagem: require('../assets/images/Robo.png') 
+      preco: '35,00',
+      imagem: require('../assets/images/Robo.png'),
+      descricao:'É um feijão preto',
+      categoria:'Feijões',
+      promocional: false,
     },
     { id: '2',
       nome: 'Moqueca', 
-      preco: 'R$ 42,00', 
-      imagem: require('../assets/images/Robo.png') 
+      preco: '42,00', 
+      imagem: require('../assets/images/Robo.png'),
+      descricao:'',
+      categoria:'Nem sei oq é isso',
+      promocional: false,
     },
     { id: '3', 
       nome: 'Churrasco', 
-      preco: 'R$ 50,00', 
-      imagem: require('../assets/images/Robo.png') 
+      preco: '50,00', 
+      imagem: require('../assets/images/Robo.png'),
+      descricao:'é tipo carne',
+      categoria:'carne',
+      promocional: false,
     },
     { id: '4', 
       nome: 'Escondidinho', 
-      preco: 'R$ 28,00', 
-      imagem: require('../assets/images/react-logo.png') 
+      preco: '28,00', 
+      imagem: require('../assets/images/react-logo.png'),
+      descricao:'shhhh',
+      categoria:'tá escondido',
+      promocional: false,
     },
     { id: '5', 
       nome: 'Lasanha', 
-      preco: 'R$ 30,00',
-      imagem: require('../assets/images/Robo.png') 
+      preco: '30,00',
+      imagem: require('../assets/images/Robo.png'),
+      descricao:'pizza',
+      categoria:'pizza',
+      promocional: true,
     },
     { id: '6', 
       nome: 'Salada', 
-      preco: 'R$ 18,00', 
-      imagem: require('../assets/images/Robo.png') 
+      preco: '18,00', 
+      imagem: require('../assets/images/Robo.png'),
+      descricao:'é verde',
+      categoria:'fôia',
+      promocional: false,
     },
     { id: '7', 
       nome: 'Hambúrguer',
-      preco: 'R$ 25,00',
-      imagem: require('../assets/images/Robo.png') 
+      preco: '25.00',
+      imagem: require('../assets/images/Robo.png'),
+      descricao:'pão',
+      categoria:'carne',
+      promocional: false,
     },
     { id: '8', 
       nome: 'Pizza',
-      preco: 'R$ 40,00',
-      imagem: require('../assets/images/Robo.png') 
+      preco: 44.98,
+      imagem: require('../assets/images/Robo.png'),
+      descricao:'pizza',
+      categoria:'pizza',
+      promocional: false,
     },
     { id: '9', 
       nome: 'Sopa',
-      preco: 'R$ 22,00', 
-      imagem: require('../assets/images/icon.png') 
+      preco: 22.5, 
+      imagem: require('../assets/images/icon.png'),
+      descricao:'líquido',
+      categoria:'caldos',
+      promocional: false,
     },
     { id: '10', 
       nome: 'Cuscuz', 
-      preco: 'R$ 20,00', 
-      imagem: require('../assets/images/Robo.png')  
+      preco: '20,00', 
+      imagem: require('../assets/images/Robo.png'),
+      descricao:'é tipo um bolo?',
+      categoria:'?',
+      promocional: true,
     },
 ];
-const pratos2 = [
+const cardapio2 = [
     { id: '1', 
       nome: 'Água',
-      preco: 'R$ 5,00',
-      imagem: require('../assets/images/Robo.png') 
+      preco: '5,00',
+      imagem: require('../assets/images/Robo.png'),
+      descricao:'',
+      categoria:'',
+      promocional: false,
     },
 ]
 
@@ -75,8 +110,89 @@ export default function TelaCardapio(){
   router.push('/pedidos');
   }
   const [dateTime, setDateTime] = useState(new Date());
-  const [pratos, setPratos] = useState([]);
+  const [cardapio, setCardapio] = useState([]);
   const [restaurantId, setRestaurantId] = useState<string | null>(null);
+
+  const [modalEditarVisible, setModalEditarVisible] = useState(false);
+  const [modalConfirmVisible, setModalConfirmVisible] = useState(false);
+  const [itemEditando, setItemEditando] = useState(null);
+  const [itemExcluindo, setItemExcluindo] = useState(null);
+
+  const [nome, setNome] = useState('');
+  const [preco, setPreco] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [categoria, setCategoria] = useState('');
+  const [promocional, setPromocional] = useState<boolean>(false);
+
+  const [imagem, setImagem] = useState<string | null>(null);
+
+  const abrirModalEditarVisible = (item) => {
+    setItemEditando(item);
+    setNome(item.nome);
+    setPreco(item.preco.toString());
+    setDescricao(item.descricao);
+    setCategoria(item.categoria);
+    setPromocional(item.promocional);
+    setImagem(item.imagem || null);
+    setModalEditarVisible(true);
+  };
+
+  const abrirModalAdicionar = () => {
+    setItemEditando(null);
+    setNome('');
+    setPreco('');
+    setDescricao('');
+    setCategoria('');
+    setPromocional(false);
+    setImagem(null);
+    setModalEditarVisible(true);
+  };
+
+  const escolherImagem = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImagem(result.assets[0].uri);
+    }
+  };
+
+  const salvarItem = () => {
+    if (!nome || !preco) return;
+
+    const novoItem = {
+      id: itemEditando ? itemEditando.id : Date.now().toString(),
+      nome,
+      preco: parseFloat(preco.replace(',', '.')),
+      imagem,
+      descricao,
+      categoria,
+      promocional
+    };
+
+    if (itemEditando) {
+      setCardapio((prev) =>
+        prev.map((item) => (item.id === novoItem.id ? novoItem : item))
+      );
+    } else {
+      setCardapio((prev) => [...prev, novoItem]);
+    }
+
+    setModalEditarVisible(false);
+  };
+
+  const confirmarExclusao = (item) => {
+    setItemExcluindo(item);
+    setModalConfirmVisible(true);
+  };
+
+  const excluirItem = () => {
+    // Colocar a lógica do banco de dados aqui
+    setCardapio((prev) => prev.filter((item) => item.id !== itemExcluindo.id));
+    setModalConfirmVisible(false);
+  };
 
   useEffect(() => {
     const id = storage.get('restaurantId');
@@ -90,8 +206,8 @@ export default function TelaCardapio(){
   useEffect(() => {
     // Aqui você puxaria os dados do banco, mas estamos simulando
     restaurantId == 'rest_a' 
-    ? setPratos(pratos1)
-    : setPratos(pratos2)
+    ? setCardapio(cardapio1)
+    : setCardapio(cardapio2)
   }, [restaurantId]);
 
   useEffect(() => {
@@ -111,36 +227,146 @@ export default function TelaCardapio(){
   
   const renderItem = ({ item }) => (
       <View style={[styles.card, { width: itemWidth, marginHorizontal: spacing / 2 }]}>
+        <View style={styles.containerBotoes}>
+          <View></View>
+          <View>
+            <Text style={{marginLeft: 70, fontSize: 20}}>{item.promocional ? 'Promocional' : ''}</Text>
+          </View>
+          <View style={styles.containerBotoes}>
+            <TouchableOpacity
+              style={styles.botaoExcluir}
+              onPress={() => confirmarExclusao(item)}
+            >
+              <Feather name="x" size={20} color="white" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.botaoEditar}
+              onPress={() => abrirModalEditarVisible(item)}
+            >
+              <Feather name="edit-2" size={15} color="white" />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.containerInformacoes}>
           <Image source={item.imagem } style={styles.imagem} />
           <Text style={styles.nome} numberOfLines={1}>{item.nome}</Text>
-          <Text style={styles.preco}>{item.preco}</Text>
+          <Text style={styles.preco}>R$ {parseFloat(item.preco).toFixed(2).replace('.', ',')}</Text>
+          <Text style={[styles.preco, {fontSize: 16}]}>{item.descricao}</Text>
+          <Text>Categoria: {item.categoria}</Text>
+        </View>
       </View>
   );
 
   return(
       <SafeAreaView style={styles.safeArea}>
-            <View style={styles.topBar}>
-              <View style={styles.side}>
-                <TouchableOpacity onPress={irParaPedidos} style={styles.button}>
-                  <Text style={styles.buttonText}>Pedidos</Text>
-                </TouchableOpacity>
+          <View style={styles.topBar}>
+            <View style={styles.side}>
+              <TouchableOpacity onPress={irParaPedidos} style={styles.button}>
+                <Text style={styles.buttonText}>Pedidos</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.centerContainer}>
+              <Feather name="calendar" size={20} color="#333" style={styles.icon} />  
+              <Text style={styles.dateText}>{formatDateTime(dateTime)}</Text>
+            </View>
+            <TouchableOpacity
+                style={styles.botaoAdicionar}
+                onPress={abrirModalAdicionar}
+              >
+              <View style={{flexDirection: 'row'}}>
+                <Text style={styles.textoBotaoAdicionar}>Adicionar Item</Text>
+                <Feather name="plus-circle" size={20} color="white" />
               </View>
-              <View style={styles.centerContainer}>
-                <Feather name="calendar" size={20} color="#333" style={styles.icon} />  
-                <Text style={styles.dateText}>{formatDateTime(dateTime)}</Text>
-              </View>
-              <View style={styles.side}></View>
+            </TouchableOpacity>
           </View>
           <Text style={styles.tituloPedidos}>Cardápio</Text>
-          <View style={styles.containerFlatList}>
-              <FlatList
-                  data={pratos}
-                  renderItem={renderItem}
-                  keyExtractor={(item) => item.id}
-                  numColumns={numColumns}
-                  contentContainerStyle={{ padding: spacing }}
-              />
-          </View>
+          <ScrollView>
+            <View style={styles.containerFlatList}>
+                <FlatList
+                    data={cardapio}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id}
+                    numColumns={numColumns}
+                />
+            </View>
+          </ScrollView>
+          {/* MODAL DE EDITAR/ADICIONAR */}
+          <Modal 
+            visible={modalEditarVisible} 
+            transparent={true} 
+            animationType="fade" 
+            onRequestClose={() => setModalEditarVisible(false)}
+          >
+            <View style={styles.overlay}>
+              <View style={styles.modal}>
+                <Text style={styles.tituloModal}>{itemEditando ? 'Editar' : 'Adicionar'} Item</Text>
+                <TextInput
+                  placeholder="Nome"
+                  style={styles.input}
+                  value={nome}
+                  onChangeText={setNome}
+                  placeholderTextColor="#aaa"
+                />
+                <TextInput
+                  placeholder="Preço"
+                  value={preco}
+                  onChangeText={setPreco}
+                  keyboardType="numeric"
+                  style={styles.input}
+                  placeholderTextColor="#aaa"
+
+                />
+                <TextInput
+                  placeholder="Descrição"
+                  value={descricao}
+                  onChangeText={setDescricao}
+                  style={styles.input}
+                  placeholderTextColor="#aaa"
+
+                />
+                <TextInput
+                  placeholder="Categoria"
+                  value={categoria}
+                  onChangeText={setCategoria}
+                  style={styles.input}
+                  placeholderTextColor="#aaa"
+
+                />
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <Text style={{fontSize: 16, marginRight: 10, paddingVertical: 10}}>Promocional</Text>
+                  <Switch
+                    value={promocional}
+                    onValueChange={(valor) => setPromocional(valor)}
+                  />
+                </View>
+                <TouchableOpacity onPress={escolherImagem} style={{ marginVertical: 10 , marginBottom: 20}}>
+                  <Text style={{ color: '#007AFF' }}>
+                    {imagem ? 'Trocar imagem' : 'Escolher imagem'}
+                  </Text>
+                </TouchableOpacity>
+                <View style={styles.modalBotoes}>
+                  <TouchableOpacity onPress={() => setModalEditarVisible(false)}>
+                    <Text style={styles.botaoCancelar}>Cancelar</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={salvarItem}>
+                    <Text style={styles.botaoSalvar}>Salvar</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </Modal>
+
+          {/* MODAL DE CANCELAR: */}
+          <ConfirmModal
+          visible={modalConfirmVisible}
+          tipo = {'excluir'}
+          message={`Tem certeza que deseja excluir o prato ${itemExcluindo?.nome}?`}
+          onConfirm={excluirItem}
+          onCancel={() => setModalConfirmVisible(false)}
+          confirmText={'Excluir'}
+          cancelText="Voltar"
+        />
+  
       </SafeAreaView>
   );
 }
@@ -179,7 +405,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   tituloPedidos: {
-     fontSize: 45,
+    fontSize: 45,
     fontWeight: 'bold',
     marginTop: 10,
     marginBottom: 10,
@@ -199,16 +425,48 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#ffffff',
   },
+  botaoAdicionar: {
+    margin: 5,
+    backgroundColor: '#10b200',
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 50,
+  },
+  textoBotaoAdicionar: {
+    fontSize: 16,
+    color: '#ffffff',
+    marginRight: 5,
+  },
   containerFlatList: {
     flex: 1,
-    paddingTop: 50,
     backgroundColor: '#fff',
+  },
+  containerBotoes: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  botaoExcluir: {
+    margin: 5,
+    backgroundColor: '#ed4141',
+    paddingHorizontal: 1,
+    borderRadius: 50,
+  },
+  botaoEditar: {
+    margin: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0892ff',
+    padding: 4,
+    borderRadius: 50,
+  },
+  containerInformacoes: {
+    alignItems: 'center',
   },
   card: {
     backgroundColor: '#f2f2f2',
     borderRadius: 8,
     overflow: 'hidden',
-    alignItems: 'center',
     marginBottom: 12,
     paddingBottom: 6,
   },
@@ -218,14 +476,70 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
   },
   nome: {
-    fontSize: 12,
+    fontSize: 20,
     fontWeight: '600',
     marginTop: 4,
     textAlign: 'center',
   },
   preco: {
-    fontSize: 11,
+    fontSize: 18,
     color: '#555',
     marginTop: 2,
   },
+  adicionar: {
+    backgroundColor: '#007AFF',
+    padding: 12,
+    alignItems: 'center',
+    marginTop: 20,
+    borderRadius: 8,
+  },
+  modalBotoes: { 
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    gap: 10 
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#aaa',
+    padding: 8,
+    borderRadius: 6,
+    marginBottom: 10,
+  },
+  tituloModal: { 
+    fontSize: 20, 
+    fontWeight: 'bold', 
+    marginBottom: 10 
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modal: {
+    backgroundColor: 'white',
+    width: '25%',
+    padding: 20,
+    borderRadius: 8,
+    elevation: 5,
+  },
+  botaoCancelar: { 
+    backgroundColor: '#ed4141',
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 5,
+    padding: 10,
+    color: '#ffffff',
+    fontSize: 16,
+  },
+  botaoSalvar: {
+    backgroundColor: '#28a745',
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 5,
+    padding: 10,
+    color: '#ffffff',
+    fontSize: 16,
+  },
+
 })
